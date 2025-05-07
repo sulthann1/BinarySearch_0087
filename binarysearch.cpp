a724dd6317fc74f111862ca25ea63264c3e1903d
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-07 14:52:14 +0700

    menambahkan void input

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -5,3 +5,20 @@ int element [10];
 int nPanjang;
 int x;
 
+void input ()
+{
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array (maksimal 10) : ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else 
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10.Silahkan coba lagi.\n";
+        }
+    }
+    
+}
