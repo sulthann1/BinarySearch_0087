f31ad5435fc313de72d928af8e3d2f865c7aa528
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-07 15:33:54 +0700

    if tidak ditemukan dalam array maka akan mengulang

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -75,8 +75,33 @@ void binarySearch()
         cout << "\n=================================\n";
         cout << "  Masukan elemen yang ingin dicari : ";
         cin >> x;
-    } while 
-    
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+           int mid = (low + high) / 2;
+           if (element[mid] == x)
+           {
+            cout << "\n [√]Elemen" << x << "ditemukan pada indeks" << mid << ".\n";
+            return;
+           } 
+           if (x < element[mid])
+           {
+                high = mid -1;
+           }
+           if (x > element[mid])
+           {
+            low =mid -1;
+           }
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n [×] Elemen " << x << "tidak ditemukan dalam Array.\n";
+        }
+        cout << "\nIngin mencari lagi "
 }
 
 
